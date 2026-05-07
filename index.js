8fa6568d0223146520ae2c5966cb754b79b6380b
Peter Lisborg <159829271+Peter-Lisborg@users.noreply.github.com>
2026-05-07 17:59:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function updateVisits() {
     const formatted = visits.toLocaleString("en");
 
     const channel = await client.channels.fetch(CHANNEL_ID);
-    await channel.setTopic(`〔🍪〕Visits: ${formatted}`);
+    await channel.setName(`〔🍪〕Visits: ${formatted}`);
 
     console.log("Updated:", formatted);
   } catch (err) {
